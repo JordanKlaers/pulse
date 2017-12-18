0085344285a5d82d14e643d7cc5cffe9b39aa928
ZiayaKens <jordanklaers@gmail.com>
2017-12-17 22:18:45 -0800

    pulse test 100 ms every 2 seconds:

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// var Gpio = require('pigpio').Gpio
+// var green = new Gpio(17, {mode: Gpio.OUTPUT})
+
+// green.pwmWrite(255); // high sends power
+console.log("high");
+
+setInterval(function() {
+  green.pwmWrite(255);
+  console.log("high");
+  setTimeout(function(){
+    green.pwmWrite(0);
+    console.log("low");
+  },100);
+}, 20000)
