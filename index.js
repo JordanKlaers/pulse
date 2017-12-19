c29539fd22ee4c65cf630307fe8f7a94fcd90d83
ZiayaKens <jordanklaers@gmail.com>
2017-12-18 17:36:13 -0800

    200ms every 7 seconds

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var green = new Gpio(17, {mode: Gpio.OUTPUT})
 green.pwmWrite(255); // high blocks
 console.log("high");
 
-// setInterval(function() {
-//   green.pwmWrite(255);
-//   console.log("high");
-//   setTimeout(function(){
-//     green.pwmWrite(0);
-//     console.log("low");
-//   },200);
-// }, 15000)
+setInterval(function() {
+  green.pwmWrite(255);
+  console.log("high");
+  setTimeout(function(){
+    green.pwmWrite(0);
+    console.log("low");
+  },200);
+}, 7000)
